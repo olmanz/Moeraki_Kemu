c33b9c641141354987e57969f8f6d91c13dab9b4
Etienne Gramlich <jim_java@gmx.de>
2015-10-23 12:36:30 +0200

    JavaDocs for Spot class

--- MoerakiKemu/src/de/htwg/se/Modellayer/Spot.java
+++ MoerakiKemu/src/de/htwg/se/Modellayer/Spot.java
@@ -2,13 +2,31 @@ package de.htwg.se.Modellayer;
 
 public class Spot {
 
-	private boolean occupied;			// True if player set token in this field
-	private String occupiedByPlayer;	// Name of the player of with token on this field
+	/**
+	 * Status of the Spot.
+	 * True if player set token in this field.
+	 */
+	private boolean occupied;
+
+	/**
+	 * Name of the player of with token on this field
+	 */
+	private String occupiedByPlayer;
 	
+	/**
+	 * Creates a new unoccupied Spot.
+	 */
 	public Spot() {
 		occupied = false;
 	}
 	
+	/**
+	 * Occupation of a Spot from a Player with a token.
+	 *
+	 * @param playerName Name of the Player.
+	 * @return True if the player successfully occupied by the player; false if
+	 * another player occupied the Spot.
+	 */
 	public boolean occupy(final String playerName) {
 		if (this.isOccupied()) {
 			return false;
@@ -18,10 +36,20 @@ public class Spot {
 		}
 	}
 
+	/**
+	 * Determines if the Spot is occupied.
+	 *
+	 * @return True of the Spot is occupied by a Player; false if not occupied.
+	 */
 	public boolean isOccupied() {
 		return occupied;
 	}
 
+	/**
+	 * Returns the name of the occupying Player as String.
+	 *
+	 * @return Name of the player.
+	 */
 	public String getOccupiedByPlayer() {
 		return occupiedByPlayer;
 	}
