b4ad5135f871b60a171d49cba95f476f3d5998a6
Etienne Gramlich <jim_java@gmx.de>
2015-10-23 09:33:19 +0200

    Attributes and Mehods for Player

--- MoerakiKemu/src/de/htwg/de/moerakikemu/Player.java
+++ MoerakiKemu/src/de/htwg/de/moerakikemu/Player.java
@@ -2,4 +2,23 @@ package de.htwg.de.moerakikemu;
 
 public class Player {
 
+	private int points;
+	private String name;
+	
+	public Player(final String name) {
+		this.name = name;
+		points = 0;
+	}
+	
+	
+	public int getPoints(){
+		return points;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	 public void addPoints(int amount){
+		 points += amount;
+	 }
 }
