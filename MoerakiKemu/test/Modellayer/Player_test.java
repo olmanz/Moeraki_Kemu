508c8e3918777899da4bf61150e2679b7acb96ad
Daniel <dh.312@t-online.de>
2015-10-23 09:46:51 +0200

    add JUnit4 and player_test

--- MoerakiKemu/test/Modellayer/Player_test.java
+++ MoerakiKemu/test/Modellayer/Player_test.java
@@ -1,5 +1,38 @@
 package Modellayer;
 
+
+
 public class Player_test {
 
+	@Before
+	public void setUp(){
+		
+	}
 }
+
+/*
+ * package Modellayer;
+
+public class Player {
+
+	private int points;
+	private String name;
+	
+	public Player(final String name) {
+		this.name = name;
+		points = 0;
+	}
+	
+	
+	public int getPoints(){
+		return points;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	 public void addPoints(int amount){
+		 points += amount;
+	 }
+}
+*/
