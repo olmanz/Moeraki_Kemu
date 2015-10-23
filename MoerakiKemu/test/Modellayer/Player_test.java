71d3ef4dbbb8eaa2844a98e25bbdafe46e8d2a08
Daniel <dh.312@t-online.de>
2015-10-23 09:54:15 +0200

    finished Player_test, all tests succeed

--- MoerakiKemu/test/Modellayer/Player_test.java
+++ MoerakiKemu/test/Modellayer/Player_test.java
@@ -1,12 +1,27 @@
 package Modellayer;
 
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
 
 public class Player_test {
+	Player player;
 
 	@Before
 	public void setUp(){
-		
+		player = new Player("alpha");
+	}
+	
+	@Test
+	public void test_getName_returnNotNullString(){
+		assertEquals("alpha", player.getName());
+	}
+	
+	@Test
+	public void test_getPoints_returnTwo(){
+		player.addPoints(2);
+		assertEquals(2, player.getPoints());
 	}
 }
 
