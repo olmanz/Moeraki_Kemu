f32afcc7046bf7d4685e7f6df3e2529c34a0324c
Etienne Gramlich <jim_java@gmx.de>
2015-11-13 12:22:10 +0100

    Cleanup for main class

--- MoerakiKemu/src/de/htwg/se/moeraki_kemu/MoerakiKemu.java
+++ MoerakiKemu/src/de/htwg/se/moeraki_kemu/MoerakiKemu.java
@@ -5,14 +5,16 @@ import de.htwg.se.moeraki_kemu.view.TextUI;
 
 public class MoerakiKemu {
 
+	private MoerakiKemu() {
+		// Private Constructor because it must not be used elsewhere
+	}
+
 	/**
 	 * Starts the game with TUI, GUI.
 	 *
 	 * @param args Unused parameters.
 	 */
 	public static void main(String[] args) {
-		// TODO Replace Comments with code
-
 		Controller controller = new Controller(12);
 
 		TextUI tui = new TextUI(controller);
