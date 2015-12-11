e798208388190db73cb8ea5203b2ee5d0f2964c9
Etienne Gramlich <jim_java@gmx.de>
2015-12-11 10:58:02 +0100

    Created new Package for GUI-Classes; Added a Class for MenuBar and MainFrame

--- MoerakiKemu/src/de/htwg/se/moerakikemu/a_view/gui/GUI.java
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/a_view/gui/GUI.java
@@ -1,13 +1,20 @@
-package de.htwg.se.moerakikemu.a_view;
+package de.htwg.se.moerakikemu.a_view.gui;
+
+import javax.swing.JFrame;
 
 import de.htwg.se.moerakikemu.a_aiview.UserInterface;
 
-public class GUI implements UserInterface {
+public class GUI extends JFrame implements UserInterface {
+	private static final long serialVersionUID = 2078463309153663728L;
+
+	public GUI() {
+		this.setJMenuBar(new MainMenu());
+	}
 
 	@Override
 	public void drawCurrentState() {
 		// TODO Auto-generated method stub
-
+		this.repaint();
 	}
 
 	@Override
---
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/a_view/gui/MainFrame.java
@@ -0,0 +1,30 @@
+package de.htwg.se.moerakikemu.a_view.gui;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	private static final long serialVersionUID = -6915328769438463935L;
+
+	JButton field[][];
+
+	public MainFrame(int fieldLength) {
+		field = new JButton[fieldLength][fieldLength];
+		for (int i = 0; i < fieldLength; i++) {
+			for (int j = 0; j < fieldLength; j++) {
+				field[i][j] = new JButton("(" + i + "/" + j + ")");
+				field[i][j].addMouseListener(
+						new MouseAdapter() {
+							public void mouseClicked(MouseEvent e) {
+								// Do some crazy stuff
+							}
+						});
+			}
+		}//END OUTTER FOR
+		
+		// Create Grid-Layout for Buttons
+	}//END CONSTRUCTOR
+}
