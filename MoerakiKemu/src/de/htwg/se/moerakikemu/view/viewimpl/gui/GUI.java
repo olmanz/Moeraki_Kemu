95ef0f60b8878140e8e97f6513f910de892e52fa
Etienne Gramlich <jim_java@gmx.de>
2015-12-26 19:51:29 +0100

    Added GUI-files that were accidentially missing in the repo

---
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/view/viewimpl/gui/GUI.java
@@ -0,0 +1,76 @@
+package de.htwg.se.moerakikemu.view.viewimpl.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import de.htwg.se.moerakikemu.controller.IController;
+import de.htwg.se.moerakikemu.view.IViewsObserver;
+import de.htwg.se.moerakikemu.view.UserInterface;
+
+public class GUI extends JFrame implements UserInterface, IViewsObserver {
+	private static final long serialVersionUID = 2078463309153663728L;
+
+	IController myController;
+	
+	JTextField messageField;
+	
+	public GUI(IController newController) {
+		super("Moeraki Kemu");
+		this.myController = newController;
+		this.messageField = new JTextField("Spiel-Informationen");
+
+		this.setJMenuBar(new MainMenu());
+		
+		this.setLayout(new BorderLayout());
+		this.add(new MainPanel(myController.getEdgeLength()), BorderLayout.CENTER);
+		this.add(messageField, BorderLayout.EAST);
+
+		this.setSize(1024, 768);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+	/*
+	void setSpotColor(final int playerNum, final int x, final  int y) {
+		field[x][y].setIcon(playerNum == 0 ? black_icon : red_icon);
+	}*/
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawCurrentState() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void queryPlayerName() {
+		// TODO Auto-generated method stub
+		String player1Name = "";
+		String player2Name = "";
+
+		while ("".equals(player1Name)) {
+			// Input Dialog
+		}
+
+		while ("".equals(player2Name)) {
+			// Input Dialog
+		}
+		
+		// Set Names
+	}
+
+	@Override
+	public void printMessage(String msg) {
+		// TODO Auto-generated method stub
+		messageField.setText(messageField.getText() + "\n" + msg);
+	}
+
+
+}
---
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/view/viewimpl/gui/MainMenu.java
@@ -0,0 +1,30 @@
+package de.htwg.se.moerakikemu.view.viewimpl.gui;
+
+import java.awt.event.KeyEvent;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MainMenu extends JMenuBar {
+	private static final long serialVersionUID = -6197107387907014280L;
+
+	JMenu game;
+	JMenuItem quit;
+
+	public MainMenu() {
+		this.setToolTipText("Easter Egg");
+		
+		// Create MenuItems
+		quit = new JMenuItem("Beenden", KeyEvent.VK_B);
+		
+		// Create and Assemble game-menu
+		game = new JMenu("Spiel");
+		game.setMnemonic(KeyEvent.VK_S);
+		game.add(quit);
+		
+		
+		// Assemle the whole MenuBar
+		this.add(game);
+	}
+}
