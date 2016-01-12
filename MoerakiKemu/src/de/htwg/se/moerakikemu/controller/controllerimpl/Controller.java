a22d467b65a41ddfb7d939ca2d2d6bd01bbe538c
Etienne Gramlich <jim_java@gmx.de>
2016-01-12 17:50:53 +0100

    Win-situation with 4 Spots as a square is correctly handeld and a new game starts

--- MoerakiKemu/src/de/htwg/se/moerakikemu/controller/controllerimpl/Controller.java
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/controller/controllerimpl/Controller.java
@@ -19,7 +19,7 @@ public class Controller extends ObserverSubject implements IController, IObserve
 	private IControllerPlayer playerController;
 	
 	private String playerWin;
-	private boolean gameEnds;
+	private boolean quitGame;
 	private boolean winner;
 	
 	public Controller(int fieldLength, IControllerPlayer playerCon) {
@@ -27,7 +27,7 @@ public class Controller extends ObserverSubject implements IController, IObserve
 		gameField = new Field(fieldLength);
 		this.fieldLength = fieldLength;
 		this.playerController = playerCon;
-		gameEnds = false;
+		quitGame = false;
 		playerWin = "";
 		notifyObservers();
 	}
@@ -157,19 +157,19 @@ public class Controller extends ObserverSubject implements IController, IObserve
 	}
 	
 	public void setEnd(boolean end) {
-		gameEnds = end;
+		quitGame = end;
 		printInfoALLUIs();
 	}
 	
 	public boolean testIfEnd(){
-		return gameEnds;
+		return quitGame;
 	}
 	
 	public void newGame(){
 		gameField = new Field(fieldLength);
 		playerController.newGame();
 		playerWin = "";
-		gameEnds = false;
+		quitGame = false;
 		winner = false;
 		
 		notifyObservers();
@@ -207,8 +207,10 @@ public class Controller extends ObserverSubject implements IController, IObserve
 	public State getState() {
 		if ("".equals(playerController.getPlayer1Name()) || "".equals(playerController.getPlayer2Name())) {
 			return State.query_player_name;
-		} else if (gameEnds) {
+		} else if (quitGame) {
 			return State.game_finished;
+		} else if (winner) {
+			return State.player_won;
 		} else {
 			return State.player_occupied;
 		}
