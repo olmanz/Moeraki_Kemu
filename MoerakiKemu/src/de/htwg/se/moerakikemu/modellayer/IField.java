5df21ac962297817bd0f9f36c1e3e3664a0bcc21
Etienne Gramlich <jim_java@gmx.de>
2016-01-13 13:59:48 +0100

    JavaDoc for Field and Player

--- MoerakiKemu/src/de/htwg/se/moerakikemu/modellayer/IField.java
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/modellayer/IField.java
@@ -2,10 +2,43 @@ package de.htwg.se.moerakikemu.modellayer;
 
 public interface IField {
 
+	/**
+	 * Tries to occupy the Spot with the given coordinates.
+	 * If another player already occupied the given Spot,
+	 * another can not re-occupy this field.
+	 * @param x The x-coordinate of the Spot to occupy.
+	 * @param y The y-coordinate of the Spot to occupy.
+	 * @param getCurrentPlayerName The name of the player that tries to occupy.
+	 * @return True if the Spot was empty and is now successfully occupied,
+	 * 			false if the Spot was already occupied.
+	 */
 	boolean occupy(int x, int y, String getCurrentPlayerName);
+	
+	/**
+	 * Determines if the Spot with the given coordinates is currently occupied.
+	 * @param x The x-coordinate of the Spot to occupy.
+	 * @param y The y-coordinate of the Spot to occupy.
+	 * @return True if the current Spot is occupied, else false.
+	 */
 	boolean getIsOccupied(int x, int y);
+	
+	/**
+	 * Returns the name of the player that occupied the given Spot.
+	 * @param x The x-coordinate of the Spot to occupy.
+	 * @param y The y-coordinate of the Spot to occupy.
+	 * @return The name of the player as String, or an empty String if no one occupied it.
+	 */
 	String getIsOccupiedFrom(int x, int y);
 	
+	/**
+	 * Determines if all Spots of the field are occupied.
+	 * @return True if all Spots are occupied, else false.
+	 */
 	boolean isFilled();
+	
+	/**
+	 * Returns the length of one edge of the (quare) field.
+	 * @return
+	 */
 	int getEdgeLength();
 }
--- MoerakiKemu/src/de/htwg/se/moerakikemu/modellayer/IPlayer.java
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/modellayer/IPlayer.java
@@ -2,10 +2,33 @@ package de.htwg.se.moerakikemu.modellayer;
 
 public interface IPlayer {
 
+	/**
+	 * Returns the number of points of the player.
+	 * @return Int >= 0;
+	 */
 	int getPoints();
+	
+	/**
+	 * Returns the name of the player.
+	 * @return String or null.
+	 */
 	String getName();
+	
+	/**
+	 * Adds an amount of points to the current amount of points.
+	 * @param amount Must be greater than 0.
+	 */
 	void addPoints(int amount);
+	
+	/**
+	 * Sets the name of a player.
+	 * @param name
+	 */
 	void setName (String name);
+	
+	/**
+	 * Sets the points to zero.
+	 */
 	void refreshPoints();
 
 }
