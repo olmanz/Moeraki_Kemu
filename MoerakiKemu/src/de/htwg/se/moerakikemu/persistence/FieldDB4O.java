a39a48754bf6f7c3ddb2a1500ce48188ffbaadfa
Oliver Manz <oliver90.manz@googlemail.com>
2017-03-23 15:39:04 +0100

    Added method to store object in db4o database

--- MoerakiKemu/src/de/htwg/se/moerakikemu/persistence/FieldDB4O.java
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/persistence/FieldDB4O.java
@@ -1,6 +1,16 @@
 package de.htwg.se.moerakikemu.persistence;
 
+import com.db4o.*;
+import de.htwg.se.moerakikemu.modellayer.IField;
 
 public class FieldDB4O {
-
+	private ObjectContainer db;
+	
+	public FieldDB4O() {
+		this.db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "field.data");
+	}
+	
+	public void saveField(final IField field) {
+		db.store(field);
+	}
 }
