c4444bfe627e1760de766e883189fff50a630901
Oliver Manz <oliver90.manz@googlemail.com>
2017-03-28 14:41:37 +0200

    Added method to delete an object by its id from the db4o database

--- MoerakiKemu/src/de/htwg/se/moerakikemu/persistence/FieldDB4O.java
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/persistence/FieldDB4O.java
@@ -1,6 +1,10 @@
 package de.htwg.se.moerakikemu.persistence;
 
+import java.util.List;
+
 import com.db4o.*;
+import com.db4o.query.Predicate;
+
 import de.htwg.se.moerakikemu.modellayer.IField;
 
 public class FieldDB4O {
@@ -12,5 +16,25 @@ public class FieldDB4O {
 	
 	public void saveField(final IField field) {
 		db.store(field);
+	}
+	
+	public void deleteFieldByID(final String id) {
+		db.delete(getFieldByID(id));
+	}
+	
+	public IField getFieldByID(final String id) {
+		List<IField> fields = db.query(new Predicate<IField>() {
+			private static final long serialVersionUID = 1L;
+			
+			public boolean match(IField field) {
+				return (id.equals(field.getID()));
+			}
+		});
+		
+		if (fields.size() > 0) {
+			return fields.get(0);
+		}
+		
+		return null;
 	}
 }
