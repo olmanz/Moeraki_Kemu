83ce607fd2c3631a6dbe6207b38218048a08f4b8
Oliver Manz <oliver90.manz@googlemail.com>
2017-04-11 14:41:52 +0200

    FieldCouchdbDAO implements IFieldDAO

--- MoerakiKemu/src/de/htwg/se/moerakikemu/persistence/couchdb/FieldCouchdbDAO.java
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/persistence/couchdb/FieldCouchdbDAO.java
@@ -1,5 +1,40 @@
 package de.htwg.se.moerakikemu.persistence.couchdb;
 
-public class FieldCouchdbDAO {
+import java.util.List;
+
+import de.htwg.se.moerakikemu.modellayer.IField;
+import de.htwg.se.moerakikemu.persistence.IFieldDAO;
+
+public class FieldCouchdbDAO implements IFieldDAO {
+
+	public void saveField(IField field) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteFieldByID(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean containsFieldByID(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public IField getFieldByID(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void generateFields(int number, int edgeLength) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<IField> getAllFields() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
