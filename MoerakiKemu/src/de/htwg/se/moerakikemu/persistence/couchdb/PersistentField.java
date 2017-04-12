6e45bd384f3e1cbf4741b84f50734fbd1088195e
Oliver Manz <oliver90.manz@googlemail.com>
2017-04-12 10:42:47 +0200

    Added getter and setter methods to PersistentField

--- MoerakiKemu/src/de/htwg/se/moerakikemu/persistence/couchdb/PersistentField.java
+++ MoerakiKemu/src/de/htwg/se/moerakikemu/persistence/couchdb/PersistentField.java
@@ -9,12 +9,52 @@ public class PersistentField extends CouchDbDocument {
 	private static final long serialVersionUID = 1538704903825440126L;
 	@TypeDiscriminator
 	private String id;
-	private Set<PersistentSpot> cells;
+	private Set<PersistentSpot> spots;
 	private String name;
 	private int blocksPerEdge;
 	private int numberSetSpots;
 	
 	public PersistentField() {
 		
+	}
+	
+	public void setID(String id) {
+		this.id = id;
+	}
+	
+	public String getID() {
+		return this.id;
+	}
+	
+	public void setSpots(Set<PersistentSpot> spots) {
+		this.spots = spots;
+	}
+	
+	public Set<PersistentSpot> getSpots() {
+		return this.spots;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setBlocksPerEdge(int blocksPerEdge) {
+		this.blocksPerEdge = blocksPerEdge;
+	}
+	
+	public int getBlocksPerEdge() {
+		return this.blocksPerEdge;
+	}
+	
+	public void setNumberSetSpots(int numberSetSpots) {
+		this.numberSetSpots = numberSetSpots;
+	}
+	
+	public int getNumberSetSpots() {
+		return this.numberSetSpots;
 	}
 }
